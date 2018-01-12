b9471ae2c1cc69a3b1cb8ab09667eea37390bc78
Vlad <tau34rus@gmail.com>
2018-01-12 19:25:40 +0300

    Project Refactoring

--- src/main/java/AppiumBased/Constants.java
+++ src/main/java/AppiumBased/Constants.java
@@ -7,8 +7,7 @@ public class Constants {
 
     static final String AppPKG = "ru.averia.collars.stg";
     static final String AppAct = "ru.averia.collars.ui.activities.SplashActivity";
-    static final String AppSource = "";
     static final Integer Timeout = 60;
-    static final File appath = new File("D:\\APK\\ru.averia.collars.stg.apk");
-    //static final File appath = new File("/opt/ru.averia.collars.stg.apk");
+    //static final File appath = new File("D:\\APK\\ru.averia.collars.stg.apk");
+    static final File appath = new File("/Users/vevseev/Public/Android/ru.averia.collars.apk");
 }
