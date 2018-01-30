0af77590ac9c9625751d3a6b43f60fc036f1e938
Vlad <tau34rus@gmail.com>
2018-01-30 11:25:14 +0300

    App Path change

--- src/main/java/AppiumBased/Constants.java
+++ src/main/java/AppiumBased/Constants.java
@@ -9,5 +9,5 @@ public class Constants {
     static final String AppAct = "ru.averia.tracker.ui.activities.SplashActivity";
     static final Integer Timeout = 60;
     static final File appath_win = new File("D:\\APK\\ru.averia.tracker.apk");
-    static final File appath_mac = new File("/Users/vevseev/Public/Android/ru.averia.tracker.apk");
+    static final File appath_mac = new File("/Users/vevseev/Public/Android/ru.averia.tracker.alpha.apk");
 }
